2f2f6225ea2e457bba2b9850ee634be451748d67
Anil Negi <negi.anil2016@gmail.com>
2022-01-23 17:21:27 +0530

    Added some styles

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes : Recipe[] = [
+    new Recipe('Test Recipe 1', 'A test desc', 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/sweet-and-sticky-tofu-with-baby-bok-choy-1642786258.jpg?crop=0.731xw:0.486xh;0.139xw,0.257xh&resize=640:*'),
+    new Recipe('Test Recipe 2', 'A test desc', 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/sweet-and-sticky-tofu-with-baby-bok-choy-1642786258.jpg?crop=0.731xw:0.486xh;0.139xw,0.257xh&resize=640:*')
+  ];
   constructor() { }
 
   ngOnInit(): void {
